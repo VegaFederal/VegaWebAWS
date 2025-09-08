ac9eafcbed2bfe9975df8c6a7c81d9a4c4d32550
Blas Urbano <79434789+Blas-U@users.noreply.github.com>
2025-09-08 15:01:12 -0400

    added the homepage banner

--- src/react-files/routes/Home.jsx
+++ src/react-files/routes/Home.jsx
@@ -78,20 +78,19 @@ const Home = () => {
         </div>
     </section>
     <section class="
-            bg-[url('./assets/Homepage_Banner.jpg')]   /* ← your image */
+            bg-[url('./assets/Homepage_first_banner.jpg')]
             bg-cover                        /* cover the entire area */
             bg-center                       /* center it */
             bg-no-repeat                    /* don’t tile it */
-            h-screen
-            md:h-auto                       /* make the section full‑viewport height */
+            py-20
             z-[-1]
             sm:overflow-x-visible overflow-x-hidden
         ">
-        <div class='flex flex-col md:flex-row md:items-center justify-center min-h-screen ml-5 md:ml-[50px] lg:ml-[8rem] gap-10 md:gap-50 px-4 '>
+        <div class='flex flex-col md:flex-row md:items-center justify-center ml-5 md:ml-[50px] lg:ml-[8rem] gap-10 md:gap-50 px-4 '>
             <h1 class=' text-white text-3xl lg:text-[52px] font-bold'>Mission First. <span class='text-secondary'>Always.</span></h1>
             <div class='flex flex-col'>
                 <p class='text-xl md:text-[28px] text-white'>Your Partner in Automation  & Commercial Adaptation</p>
-                <p class='text-xl md:text-[24px] text-white mt-[50px]'>We don’t sell one-size-fits-all platforms.</p>
+                <p class='text-xl md:text-[24px] text-white mt-[50px]'>We don't sell one-size-fits-all platforms.</p>
                 <p class='text-xl md:text-[24px] text-white font-bold mt-[30px]'>We build the right solution for the mission at hand.</p>
             </div>
         </div>
