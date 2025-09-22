6ad330673494dc19caf3e6a9059dd6cfefe83052
Robbie Allen <Robbie@vegafederal.com>
2025-09-22 09:59:10 -0500

    removed spacing form inbetween the pictures and words on the home cards

--- src/react-files/components/Homepage_Cards.jsx
+++ src/react-files/components/Homepage_Cards.jsx
@@ -2,7 +2,7 @@ export default function Homepage_Cards({image, title, description_one, descripti
     return (
         <div class='flex flex-col gap-5 w-100 md:h-120 lg:h-150 2xl:h-160 md:w-full bg-primary'>
             <img class='' src={image} alt={title}/>
-            <h7 class='text-[20px] md:text-xl font-bold ml-[20px] text-secondary mb-[20px] md:mb-0 mt-[-7rem] md:mt-0 2xl:mt-[-60px]'>{title}</h7>
+            <h7 class='text-[20px] md:text-xl font-bold ml-[20px] text-secondary mb-[20px] md:mb-0 mt-[-7rem] md:mt-0 2xl:mt-[-130px]'>{title}</h7>
             <p class='text-[16px] md:text-md ml-[20px] text-white lg:mb-[20px] font-bold'>{description_one}</p>
             <p class='text-[16px] md:text-md ml-[20px] text-white mb-10 md:mb-20 lg:mb-[60px] font-bold'>{description_two}</p>
         </div>
