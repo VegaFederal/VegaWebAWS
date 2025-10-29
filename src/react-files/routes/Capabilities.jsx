5f1d6e99761f423ab1e42b0e8bdc4d35d4ed3384
Robbie Allen <Robbie@vegafederal.com>
2025-10-29 09:49:02 -0500

    Added some spacing for Mobile Application Development

--- src/react-files/routes/Capabilities.jsx
+++ src/react-files/routes/Capabilities.jsx
@@ -98,7 +98,7 @@ const Capabilities = () => {
             sm:overflow-x-visable overflow-x-hidden
             
         ">
-        <div class='flex flex-col lg:flex-row xl:flex-row md:flex-col  md:items-center items-center min-h-fit ml-5 md:ml-[50px] lg:pl-[1.5rem] gap-10 md: px-4'>
+        <div class='flex flex-col lg:flex-row xl:flex-row md:flex-col  md:items-center items-center min-h-fit ml-5 md:ml-[50px] lg:pl-[1.5rem] gap-10 md: px-4 pt-10'>
             <div class='md:mb-0 xl:mb-15 2xl:ml-[6.25rem]'>
                 <h1 class='text-2xl md:text-4xl font-bold ml-5  md:pt-0 pt-10'>Mobile Application Development</h1>
                 <h3 class='text-2xl md:text-3xl text-secondary ml-5  md:pt-5 pt-5'>Mobile. Performance. Security.</h3>
@@ -142,7 +142,7 @@ const Capabilities = () => {
             sm:overflow-x-visable overflow-x-hidden
             
         ">
-        <div class='flex flex-col lg:flex-row xl:flex-row md:flex-col  md:items-center items-center min-h-fit ml-5 md:ml-[50px] lg:pl-[2rem] gap-10  px-4 pt-10 '>
+        <div class='flex flex-col lg:flex-row xl:flex-row md:flex-col  md:items-center items-center min-h-fit ml-5 md:ml-[50px] lg:pl-[2rem] gap-10  px-4 pt-10'>
             <div class='md:mb-0 xl:mb-15 lg:ml-0 2xl:ml-[6rem]'>
                 <h1 class='text-2xl md:text-4xl font-bold ml-5  md:pt-0 pt-10'>Platform & Enterprise Services</h1>
                 <h3 class='text-2xl md:text-3xl text-secondary ml-5  md:pt-5 pt-5'>Optimizing Infrastructure. Enabling Scale.</h3>
