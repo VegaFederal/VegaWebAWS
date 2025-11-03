add79ed6550e9e6a9ac9db304354881d6847eaad
Blas Urbano <79434789+Blas-U@users.noreply.github.com>
2025-11-02 22:22:50 -0500

    update

--- src/react-files/components/Footer.jsx
+++ src/react-files/components/Footer.jsx
@@ -5,12 +5,12 @@ export default function Footer(){
     return (
         <section class='bg-primary bg-cover bg-center lg:h-screen xl:h-fit pl-[10px] mr-[10px]'>
 
-            <div class='flex flex-col pt-40 lg:ml-[10rem] xl:ml-[100px] gap-5'>
+            <div class='flex flex-col pt-40 ml-10 sm:ml-[10rem] lg:ml-[10rem] xl:ml-[100px] gap-5'>
 
                 <h5 class='text-[32px] font-bold text-white'>Vega | Automation. Adaptation. Mission First.</h5>
                 <h1 class='text-[28px] text-white mb-12'>Proudly based in St. Louis, serving clients nationwide.</h1>
             </div>
-            <div class='flex flex-col lg:ml-[10rem] xl:ml-[100px] pb-30'>
+            <div class='flex flex-col ml-10 sm:ml-[10rem] lg:ml-[10rem] xl:ml-[100px] pb-30'>
                 <p class='text-xl text-white'>T REX Center</p>
                 <p class='text-xl text-white'>911 Washington Ave</p>
                 <p class='text-xl text-white'>St. Louis, MO 63101</p>
