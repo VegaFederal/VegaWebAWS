62d3fa223d114982294283a7cc65325235dc9d45
Robbie Allen <Robbie@vegafederal.com>
2025-11-10 10:20:26 -0600

    removed all unneeded files and code from app.jsx

--- src/react-files/App.jsx
+++ src/react-files/App.jsx
@@ -16,15 +16,6 @@ function App() {
       <Route path="/Our_Story" element={<Our_Story/>}/>
       <Route path="/Careers" element={<Careers_Main/>}></Route>
       <Route path="/About" element={<About_Updated/>}></Route>
-      <Route path="/About_two" element={<About_concept/>}></Route>
-      <Route path="/Copy" element={<Copy/>}></Route>
-      <Route path="/Copy_Two" element={<Copy_Two/>}></Route>
-      <Route path="Copy_Three" element={<Copy_Three/>}></Route>
-      <Route path="Copy_Four" element={<Copy_Four/>}></Route>
-      <Route path="/Copy_Five" element={<Copy_Five/>}></Route>
-      <Route path="/Copy_Six" element={<Copy_Six/>}></Route>
-      <Route path="/Copy_Seven" element={<Copy_Seven/>}></Route>
-      <Route path="/Copy_Eight" element={<Copy_Eight/>}></Route>
       </Routes>
     </>
   )
