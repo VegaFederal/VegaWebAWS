d1194b934b66373232edde322b70720dfc0dcf4e
Robbie Allen <Robbie@vegafederal.com>
2026-01-19 13:14:12 -0600

    Updated contact info on footer

--- src/react-files/components/Footer.jsx
+++ src/react-files/components/Footer.jsx
@@ -10,11 +10,11 @@ export default function Footer(){
             <img src={stlSkyline} alt='' className="object-contain w-full"/>
             <div class='flex flex-col pt-40 ml-10 sm:ml-[10rem] lg:ml-[10rem] xl:ml-[100px] gap-5'>
 
-                <h5 class='text-[32px] font-bold text-white'>Vega | Automation. Adaptation. Mission First.</h5>
-                <h1 class='text-[28px] text-white mb-12'>Proudly based in St. Louis, serving clients nationwide.</h1>
+                <h1 class='text-[32px] font-bold text-white'>Vega Federal Solutions | Automation. Adaptation. Mission First.</h1>
+                <h2 class='text-[28px] text-white mb-12'>Proudly based in St. Louis, serving clients nationwide.</h2>
             </div>
             <div class='flex flex-col ml-10 sm:ml-[10rem] lg:ml-[10rem] xl:ml-[100px] pb-30'>
-                <p class='text-xl text-white'>T REX Center</p>
+                <p class='text-xl text-white'>T-REX Innovation Center</p>
                 <p class='text-xl text-white'>911 Washington Ave</p>
                 <p class='text-xl text-white'>St. Louis, MO 63101</p>
             </div>
