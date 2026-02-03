53e5a64a29858f50e710f0830d665cf2dc3f170a
marklopervega <mark@vegafederal.com>
2026-02-03 14:44:21 -0500

    Added Linked in back in.

--- src/react-files/components/Footer.jsx
+++ src/react-files/components/Footer.jsx
@@ -17,6 +17,12 @@ export default function Footer(){
                     <p className='text-xl text-white'>911 Washington Ave, Suite 412</p>
                     <p className='text-xl text-white'>St. Louis, MO 63101</p>
                     <Link to='mailto:Info@vegafederal.com' className='footer-link'>Info@vegafederal.com</Link>
+                    <Link
+                        to='https://www.linkedin.com/company/vega-fed-solutions/posts/?feedView=all'
+                        className='footer-link'
+                    >
+                    LinkedIn
+                </Link>
                 </div>
             </div>
         </section>
