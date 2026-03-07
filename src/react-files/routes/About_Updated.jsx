a1fa771169804a9a6dad5a33a92e2474c88c8b57
Blas Urbano <79434789+Blas-U@users.noreply.github.com>
2026-03-07 09:14:34 -0500

    Update

--- src/react-files/routes/About_Updated.jsx
+++ src/react-files/routes/About_Updated.jsx
@@ -2,7 +2,6 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer';
 import SkipLink from '../components/SkipLink'
 
-
 import {teamMembers} from '../Team_members.js'
 const About_Updated = () => {
     return(
