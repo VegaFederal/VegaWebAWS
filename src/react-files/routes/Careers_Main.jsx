26db1f1e4500ed08ac684e008fca97acaeedfa92
Blas Urbano <79434789+Blas-U@users.noreply.github.com>
2026-03-09 10:47:41 -0400

    update

--- src/react-files/routes/Careers_Main.jsx
+++ src/react-files/routes/Careers_Main.jsx
@@ -5,21 +5,17 @@ const Careers_Main = () => {
     return(
         <>
         <SkipLink />
-            <header className="fixed top-0 left-0 right-0 z-50 bg-white shadow-sm" style={{ height: '40px' }}>
-                <div>
-                    <Navbar color_career='text-secondary'/>
-                </div>
-            </header>
+            <Navbar />
 
-            <div id="after-navbar" tabIndex={-1}>
+            <div id="after-navbar" tabIndex={-1} style={{ paddingTop: '56px' }}>
                 <main id="main-content" tabIndex={-1}>
                 {/* Full page iframe with adjusted margin */}
-                <div style={{ marginTop: '-30px' }}>
+                <div>
                     <iframe 
                         src="https://www.careers-page.com/vega" 
                         style={{
                             width: '100%', 
-                            height: 'calc(100vh - 20px)', 
+                            height: 'calc(100vh - 56px)', 
                             border: 'none'
                         }}
                         title="Vega Careers Portal"
