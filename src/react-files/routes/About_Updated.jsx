2f42cc9f8dafe3a3ba583a7230df7ae3d7755d43
Blas Urbano <79434789+Blas-U@users.noreply.github.com>
2026-06-04 14:37:23 -0400

    update adding fetching from the about_updated page

--- src/react-files/routes/About_Updated.jsx
+++ src/react-files/routes/About_Updated.jsx
@@ -2,16 +2,26 @@ import { useRef, useEffect, useState, useLayoutEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import SkipLink from '../components/SkipLink'
-import { teamMembers } from '../Team_members.js'
+//import { teamMembers } from '../Team_members.js'
 import './About_Updated.css'
 
 const About_Updated = () => {
   const gridRef = useRef(null)
   const [visibleCards, setVisibleCards] = useState(new Set())
+  const [teamMembers, setTeamMembers] = useState([])
 
   useLayoutEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
   }, [])
+
+  useEffect(() => {
+    fetch('https://7vwom90zl0.execute-api.us-east-1.amazonaws.com/team-members')
+      .then(response => response.json())
+      .then(data => {
+        setTeamMembers(data)
+      })
+      .catch(error => console.error('Error:', error));
+  }, [teamMembers]);
 
   /* Lazy reveal: observe each card, add visible class when in viewport */
   useEffect(() => {
